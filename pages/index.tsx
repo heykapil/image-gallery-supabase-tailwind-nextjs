ac0e7f0c63231cfee6a38b2729aa7e12eb3b5333
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2023-01-05 15:14:19 +0530

    back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
-import BlurImage from '../components/BlurImage'
 export async function getStaticProps() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
@@ -40,3 +39,28 @@ export default function Gallery({ images }: { images: Image[] }) {
   )
 }
 
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+ )
+}
