eb98ac70d8b0894056a7d622e3174421cedd3558
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2023-01-05 15:03:51 +0530

    .

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -30,7 +30,6 @@ export default function BlurImage({ image }: { image: Image }) {
     const [isLoading, setLoading] = useState(true);
   
     return (
-    {images.map((image) => (
       <a href={image.href} className="group">
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
           <Image
@@ -50,6 +49,6 @@ export default function BlurImage({ image }: { image: Image }) {
         <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
         <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
       </a>
-    ))})
+   )
   }
   
